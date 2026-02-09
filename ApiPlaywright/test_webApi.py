fe8b006f03884871faadb214d5b73cca8feda026
RubenMazaTester <rubenmorales_9@hotmail.com>
2026-02-08 19:34:51 -0700

    Hybrid examples of web and API testing

---
+++ ApiPlaywright/test_webApi.py
@@ -0,0 +1,50 @@
+import time
+
+from playwright.sync_api import Playwright, expect
+
+from Utils.ApiBase import APIutils
+
+
+def test_e2ewebApi(playwright: Playwright):
+
+    #This is a hard coded example of the login process
+    """browser=playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+
+    page.goto("https://rahulshettyacademy.com/client")
+
+    to start this test, we want to create an order through API requests exclusively,
+    after that, we will validate everything through UI validatiom
+
+
+    #we landed on the page, let´s login
+    page.get_by_placeholder("email@example.com").fill("batman@hotmail.com")#user
+    page.get_by_placeholder("enter your passsword").fill("@Batmanshetty1")#pass
+    page.get_by_role("button",name="login").click()"""
+
+
+    """now, we have already define the api utilities in another page od this project, we are 
+    implementating page object model"""
+
+    #I imported the class APIutils to use its methos
+
+    #and we will make an object of it to use the class methods from the other file
+    apiutils = APIutils()
+
+    #Here we are logging in without even seeing the UI
+    apiutils.getToken(playwright)
+
+    """Then, we can create an order, because we need a token that is generated after log in
+    because, the create order request needs an authorization token, we will add it as a header
+    """
+    orderID = apiutils.createOrder(playwright)
+    print(orderID)
+    #now we validate that order ID refreshed in the orders page
+    page=apiutils.getToken(playwright)
+    page.get_by_role("button",name="ORDERS").click()
+    row= page.locator("tr").filter(has_text=orderID)
+    row.get_by_role("button",name="View").click()
+    #expect(page.locator(".tagline"))to_contain_text("Thank you for Shopping With Us")
+    expect(page.locator(".tagline")).to_contain_text("Thank you for Shopping With Us")
+    context.close()
---
+++ Utils/ApiBase.py
@@ -0,0 +1,32 @@
+import playwright
+from playwright.sync_api import Playwright
+
+loginPayload = {"userEmail": "batman@hotmail.com", "userPassword": "@Batmanshetty1"}
+
+requesPayload={"orders": [{"country": 'Guatemala', "productOrderedId": '6960ea76c941646b7a8b3dd5'}]}
+
+token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2OTg5MmRlZGRjNDBiNDhmMTJjN2Q4OTIiLCJ1c2VyRW1haWwiOiJiYXRtYW5AaG90bWFpbC5jb20iLCJ1c2VyTW9iaWxlIjoxMjM0NTY3ODkxLCJ1c2VyUm9sZSI6ImN1c3RvbWVyIiwiaWF0IjoxNzcwNTk4NjUxLCJleHAiOjE4MDIxNTYyNTF9.PhsdmgONvji7PcGOK80kPoYMpzZuo1VThOqBHS_hU9g"
+
+class APIutils:
+
+    def getToken(self,playwright:Playwright):
+        api_request_context = playwright.request.new_context(base_url="https://rahulshettyacademy.com")
+        loginresponse = api_request_context.post("https://rahulshettyacademy.com/api/ecom/auth/login", data=loginPayload)
+
+        assert loginresponse.ok
+        print(loginresponse.json())
+        responseBody = loginresponse.json()
+        return responseBody["token"]
+
+    def createOrder(self, playwright:Playwright):
+        token = self.getToken(playwright)
+        api_request_context= playwright.request.new_context(base_url = "https://rahulshettyacademy.com")
+        response = api_request_context.post("api/ecom/order/create-order", data=requesPayload,
+                                 headers={"Authorization" : token , "content-type" : "application/json"})
+        responseID = response.json()
+        print(responseID)
+
+        return responseID["orders"][0]
+
+
+
